573de90eda11e9f46f28c882ef8ed4c08589ee1b
mmtomczak <mmtomczakk@gmail.com>
2024-04-15 22:19:31 +0200

    refactoring, changed description button style

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from database.db_filter import get_data, filter_data
 app = Flask(__name__)
 app.secret_key = 'secretkey'
 
+
 # Create crawl process
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
     'ITEM_PIPELINES': {'moviescraper.moviescraper.pipelines.MoviescraperPipeline': 300}
 })
+
 
 @app.route('/', methods=['GET', 'POST'])
 def type():
@@ -20,6 +22,7 @@ def type():
         session['selected_type'] = request.form.get('type')
         return redirect(url_for('genres'))
     return render_template('type.html', types_links_dict=types_links_dict)
+
 
 @app.route('/genres', methods=['GET', 'POST'])
 def genres():
@@ -30,6 +33,7 @@ def genres():
         return redirect(url_for('scraping'))
     return render_template('genres.html', genres=genres)
 
+
 @app.route('/scraping', methods=['GET', 'POST'])
 def scraping():
     selected_type = session.get('selected_type')
@@ -39,6 +43,7 @@ def scraping():
     process.start()
     return redirect(url_for('browse'))
 
+
 @app.route('/browse', methods=['GET', 'POST'])
 def browse():
     title_filter = request.args.get('title', None)
@@ -47,6 +52,7 @@ def browse():
     is_movie = session.get('selected_type') == '/movie?language=en'
     filtered_data = filter_data(data, is_movie, title_filter, year_filter)
     return render_template('browse.html', data=filtered_data, is_movie=is_movie)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
